912326eea9bbd20f0218f8ccde770d5524d62eb8
ting-xuan <a9523023650@gmail.com>
2021-11-19 19:26:03 +0800

    1119-1926

--- week_4_rpg/Ladder/src/app.ts
+++ week_4_rpg/Ladder/src/app.ts
@@ -13,25 +13,50 @@ document.querySelector("#app")?.appendChild(app.view);
 let lineNum = 5;
 const line = new PIXI.Graphics();
 const lineStartX = 100;
-for (let index = 0; index < lineNum; index++) {
+const color = [0xffc107, 0x0d6efd, 0x198754, 0xfd7e14];
+
+const point: unknown[] = [];
+// 先畫 直線
+for (let i = 0; i < lineNum; i++) {
   // x = 100,300,500,700,900
-  // 先畫 直線
-  let spaceX = lineStartX + (index * app.screen.width) / lineNum;
+  let spaceX = lineStartX + (i * app.screen.width) / lineNum;
   line.lineStyle(5, 0xaaaaaa);
   line.moveTo(spaceX, 0);
   line.lineTo(spaceX, app.screen.height);
+  let obj = {
+    x: spaceX,
+    y: app.screen.height,
+  };
   app.stage.addChild(line);
-  // 再畫橫線
-  // y = 100 260 420 580 740
-  let spaceY = lineStartX + (index * app.screen.height) / lineNum;
-  console.log(spaceY);
+  point.push(obj);
+}
+//console.log(point);
 
-  line.moveTo(lineStartX, spaceY);
-  line.lineTo(app.screen.width - lineStartX, spaceY);
+// 再畫橫線
+for (let i = 0; i < lineNum; i++) {
+  // y = 100 260 420 580 740
+  let spaceY = lineStartX + (i * app.screen.height) / lineNum;
+
+  line.lineStyle(5, color[0]);
+  line.moveTo(lineStartX, spaceY); // (100,100)
+  line.lineTo(300, spaceY);
+
+  spaceY -= 50;
+  line.lineStyle(5, color[1]);
+  line.moveTo(lineStartX + 200, spaceY); // (100,100)
+  line.lineTo(500, spaceY);
   app.stage.addChild(line);
 }
 
 const circle = new PIXI.Graphics();
-circle.lineStyle(5, 0xaaaaaa);
-circle.drawCircle(50, 50, 20);
+circle.beginFill(0x0dcaf0);
+circle.drawCircle(100, 0, 20);
+
+app.ticker.add((time) => {
+  circle.y += time;
+  if (circle.y >= app.screen.height) {
+    app.ticker.stop();
+  }
+});
+
 app.stage.addChild(circle);
