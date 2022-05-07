67385b284afa433c9c024449dd3f9a5591ace05f
kimn <a9523023650@gmail.com>
2022-05-07 19:14:19 +0800

    [新增] 魔瞳線型

--- week_1_pixi/pixi-vite/src/app.ts
+++ week_1_pixi/pixi-vite/src/app.ts
@@ -3,113 +3,123 @@ import gsap from "gsap";
 import { Spine } from "pixi-spine";
 import * as PIXI from "pixi.js";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-
-const style = {
-  width: 1200,
-  height: 600,
-  antialias: true,
-  resolution: 1,
-  backgroundColor: 0x282c34,
-};
+import pointValues from "./pointValues";
+gsap.registerPlugin(MotionPathPlugin);
 
 class App extends PIXI.Application {
-  _resources: Dict<PIXI.LoaderResource>;
-  _pointsArray: PIXI.Point[][] = [];
+    _resources: Dict<PIXI.LoaderResource>;
+    _pointsArray: PIXI.Point[][] = [];
 
-  constructor() {
-    super(style);
-    this._init();
-  }
-
-  async _init(): Promise<void> {
-    await new Promise((resolve) => {
-      document.querySelector("#app").appendChild(this.view);
-      this.loader
-        .add("red", "../img/pic_main_line_1.png")
-        .add("yellow", "../img/pic_main_line_6.png")
-        .add("blue", "../img/pic_main_line_5.png")
-        .add("coin", "../export/coin-pro.json")
-        .add("mask", "../img/pic_mg_reelMaskSmall.png")
-        .load((loader, resources) => {
-          this._resources = resources;
-        })
-        .load(resolve);
-    });
-    for (let i = 0; i < 4; i++) {
-      this.stage.addChild(this._createLine());
+    constructor() {
+        super({
+            width: 1200,
+            height: 1000,
+            antialias: true,
+            resolution: 1,
+        });
+        this._init();
     }
-    this._startShow();
-  }
 
-  _createLine(): PIXI.Container {
-    const box = new PIXI.Container();
-    const points = this._createPoints();
-    this._pointsArray.push(points);
-    const yellow = this._createLineBox(points, "blue");
-    const red = this._createLineBox(points, "red");
-    box.addChild(yellow, red);
-    return box;
-  }
+    async _init(): Promise<void> {
+        await new Promise((resolve) => {
+            document.querySelector("#app").appendChild(this.view);
+            this.loader
+                .add("red", "../img/pic_main_line_1.png")
+                .add("yellow", "../img/pic_main_line_6.png")
+                .add("blue", "../img/pic_main_line_5.png")
+                .add("coin", "../export/coin-pro.json")
+                .add("mask", "../img/pic_mg_reelMaskSmall.png")
+                .add("line", "../img/FX_Line-2.png")
+                .load((loader, resources) => {
+                    this._resources = resources;
+                })
+                .load(resolve);
+        });
 
-  _createLineBox(points: PIXI.Point[], style: string): PIXI.Container {
-    const container = new PIXI.Container();
-    for (let i = 0; i < points.length - 1; i++) {
-      const { x, y } = points[i + 1];
-
-      const rope = new PIXI.SimpleRope(PIXI.Texture.from(style), [
-        points[i],
-        new PIXI.Point(x + 2, y),
-      ]);
-      container.addChild(rope);
+        this.stage.addChild(this._createLine());
     }
-    return container;
-  }
 
-  _createPoints(): PIXI.Point[] {
-    const ropeLength = this.view.width / 6;
-    const points = [];
-    for (let i = 0; i <= 6; i++) {
-      const index = Math.floor(Math.random() * 3);
-      points.push(new PIXI.Point(i * ropeLength, [100, 300, 500][index]));
+    _createLine(): PIXI.Container {
+        const box = new PIXI.Container();
+        const points = this._createPoints();
+        this._pointsArray.push(points);
+
+        const yellow = this._createLineBox(points, "line");
+        box.addChild(yellow);
+        return box;
     }
-    return points;
-  }
 
-  async _startShow(): Promise<void> {
-    const array = [...this.stage.children].reverse() as PIXI.Container[];
-    const pointsArray = [...this._pointsArray].reverse();
-    for (let i = 0; i < array.length; i++) {
-      const container = array[i];
-      const red = container.children[1];
-      const mask = PIXI.Sprite.from("mask");
-      mask.scale.y = 1.5;
-      mask.anchor.y = 0.5;
-      red.mask = mask;
-      const coin = this._createSpine();
-      const { x, y } = pointsArray[i][0];
-      coin.position.set(x, y);
-      mask.position.set(x, y);
-      container.addChild(mask, coin);
-      const duration = 3;
-      gsap.registerPlugin(MotionPathPlugin);
-      await gsap.to([coin, mask], {
-        motionPath: { path: pointsArray[i], curviness: 0 },
-        duration,
-        ease: "none",
-      });
-      container.removeChild(red, mask);
-      container.visible = false;
+    _createLineBox(pointsA: PIXI.Point[], style: string): PIXI.Container {
+        const [{ x, y }] = this._pointsArray[0];
+        const points = Array.from({ length: 100 }, () => ({ x, y } as PIXI.Point));
+        const container = new PIXI.Container();
+        const rope = new PIXI.SimpleRope(PIXI.Texture.from(style), points);
+        container.addChild(rope);
+        this.showA(points);
+        return container;
     }
-    array.forEach((item) => (item.visible = true));
-  }
 
-  _createSpine(): Spine {
-    const coin = new Spine(this._resources["coin"].spineData);
-    coin.scale.set(0.2);
-    coin.state.setAnimation(0, "animation", true);
-    coin.state.timeScale = 3;
-    return coin;
-  }
+    showA(points: PIXI.Point[]) {
+        const duration = 1;
+        gsap.to(points, {
+            motionPath: { path: this._pointsArray[0], curviness: 0.5 },
+            duration,
+            ease: "none",
+            stagger: 0.01 * duration,
+            repeat: -1,
+        });
+    }
+
+    _createPoints(): PIXI.Point[] {
+        const ropeLength = this.view.width / 6;
+        const points = [];
+        const graphics = new PIXI.Graphics();
+        graphics.beginFill(0xde3249);
+        for (let i = 0; i <= 6; i++) {
+            const index = Math.floor(Math.random() * 3);
+            points.push(new PIXI.Point(i * ropeLength, [100, 300, 500][index]));
+            graphics.drawCircle(i * ropeLength, [100, 300, 500][index], 5);
+        }
+        graphics.endFill();
+        this.stage.addChild(graphics);
+        return points;
+    }
+
+    async _startShow(): Promise<void> {
+        const array = [...this.stage.children].reverse() as PIXI.Container[];
+        const pointsArray = [...this._pointsArray].reverse();
+        for (let i = 0; i < array.length; i++) {
+            const container = array[i];
+            const red = container.children[1];
+            const mask = PIXI.Sprite.from("mask");
+            mask.scale.y = 1.5;
+            mask.anchor.y = 0.5;
+            red.mask = mask;
+            const coin = this._createSpine();
+            const { x, y } = pointsArray[i][0];
+            coin.position.set(x, y);
+            mask.position.set(x, y);
+            container.addChild(mask, coin);
+            const duration = 3;
+            gsap.registerPlugin(MotionPathPlugin);
+            await gsap.to([coin, mask], {
+                motionPath: { path: pointsArray[i], curviness: 0 },
+                duration,
+                ease: "none",
+            });
+            container.removeChild(red, mask);
+            container.visible = false;
+        }
+        array.forEach((item) => (item.visible = true));
+    }
+
+    _createSpine(): Spine {
+        const coin = new Spine(this._resources["coin"].spineData);
+        coin.scale.set(0.2);
+        coin.state.setAnimation(0, "animation", true);
+        coin.state.timeScale = 3;
+        return coin;
+    }
 }
 
 export default App;
---
+++ week_1_pixi/pixi-vite/src/snake.ts
@@ -0,0 +1,61 @@
+import * as PIXI from "pixi.js";
+import gsap from "gsap";
+import pointValues from "./pointValues";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+
+const app = new PIXI.Application({
+    width: 1200,
+    height: 1000,
+    antialias: true,
+    transparent: true,
+});
+document.querySelector("#app").appendChild(app.view);
+
+gsap.registerPlugin(MotionPathPlugin);
+const newTweenPoint: { x: number; y: number }[] = [];
+let switcher = false;
+
+const _movePos = { x: 0, y: 0 };
+
+gsap.to(_movePos, {
+    duration: 5,
+    motionPath: { path: pointValues, curviness: 0 },
+    onUpdate: () => {
+        //When Counting...Push new value to new array
+        newTweenPoint.push({ x: _movePos.x, y: _movePos.y });
+    },
+    onComplete: () => {
+        switcher = true; //When done run Animation
+        console.log(newTweenPoint);
+    },
+});
+
+const ropePoints: { x: number; y: number }[] = [];
+for (let i = 0; i < 80; i = i + 1) {
+    ropePoints.push(pointValues[i]);
+}
+const imgSrc = PIXI.Texture.from("http://scottmcdonnell.github.io/pixi-examples/_assets/snake.png");
+const strip = new PIXI.SimpleRope(imgSrc, ropePoints);
+strip.x = -459;
+
+const snakeContainer = new PIXI.Container();
+snakeContainer.position.set(250, 300);
+snakeContainer.scale.set(0.5);
+snakeContainer.addChild(strip);
+app.stage.addChild(snakeContainer);
+
+let timer = 0;
+
+app.ticker.add(() => {
+    if (switcher) {
+        if (timer >= newTweenPoint.length - 80) {
+            timer = 0; //repeat
+        }
+
+        for (let i = 0; i < ropePoints.length; i++) {
+            ropePoints[i].x = newTweenPoint[i + timer].x;
+            ropePoints[i].y = newTweenPoint[i + timer].y;
+        }
+        timer++;
+    }
+});
