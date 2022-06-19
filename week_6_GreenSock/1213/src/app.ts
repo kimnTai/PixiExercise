cede83dd167cccac4719b50546f64abd45c4cd02
kimn <a9523023650@gmail.com>
2022-06-19 15:03:27 +0800

    spine 練習

--- week_6_GreenSock/1213/src/app.ts
+++ week_6_GreenSock/1213/src/app.ts
@@ -1,13 +1,32 @@
 import * as PIXI from "pixi.js-legacy";
 import { State, StateSystem } from "pixi.js-legacy";
+import dragon from "./example/dragon";
 import { monster } from "./example/monster";
+import { Spine } from "pixi-spine";
 (window as any).__PIXI_INSPECTOR_GLOBAL_HOOK__ &&
-  (window as any).__PIXI_INSPECTOR_GLOBAL_HOOK__.register({ PIXI: PIXI });
+    (window as any).__PIXI_INSPECTOR_GLOBAL_HOOK__.register({ PIXI: PIXI });
 
 export const app = new PIXI.Application({
-  width: 800,
-  height: 600,
+    width: 800,
+    height: 600,
 });
 document.querySelector("#app").appendChild(app.view);
 
-app.loader.add("monster", "../assets/monster/monster.json").load(monster);
+app.loader.add("dragon", "../assets/dragon/dragon.json").load(async (loader: PIXI.Loader, res: any) => {
+    // 實例化脊椎動畫
+    const dragon = new Spine(res.dragon.spineData);
+    // 設置動畫播放
+    //dragon.state.setAnimation(0, "flying", false);
+    dragon.state.addAnimation(0, "flying", false, 0);
+    // 測量脊椎動畫並將其放置在其容器內以將其與原點對齊
+    const localRect = dragon.getLocalBounds();
+    dragon.position.set(-localRect.x, -localRect.y);
+    // 為脊椎動畫創建一個容器並將動畫添加到其中
+    const container = new PIXI.Container();
+    container.scale.set(0.5);
+    container.addChild(dragon);
+    app.stage.addChild(container);
+    await new Promise((complete) => dragon.state.addListener({ complete }));
+    dragon.state.clearListeners();
+    console.log(dragon);
+});
--- week_6_GreenSock/1213/src/example/dragon.ts
+++ week_6_GreenSock/1213/src/example/dragon.ts
@@ -2,19 +2,25 @@ import * as PIXI from "pixi.js-legacy";
 import { Spine } from "pixi-spine";
 import { app } from "../app";
 
-function dragon(loader: PIXI.Loader, res: any) {
-  // 實例化脊椎動畫
-  const dragon = new Spine(res.dragon.spineData);
-  // 設置動畫播放
-  dragon.state.setAnimation(0, "flying", true);
-  // 測量脊椎動畫並將其放置在其容器內以將其與原點對齊
-  const localRect = dragon.getLocalBounds();
-  dragon.position.set(-localRect.x, -localRect.y);
+export default function dragon(loader: PIXI.Loader, res: any) {
+    // 實例化脊椎動畫
+    const dragon = new Spine(res.dragon.spineData);
+    // 設置動畫播放
+    //dragon.state.setAnimation(0, "flying", false);
+    dragon.state.addAnimation(0, "flying", false, 0);
+    dragon.state.addListener({
+        complete: (entry) => {
+            console.log(entry);
+        },
+    });
+    // 測量脊椎動畫並將其放置在其容器內以將其與原點對齊
+    const localRect = dragon.getLocalBounds();
+    dragon.position.set(-localRect.x, -localRect.y);
 
-  // 為脊椎動畫創建一個容器並將動畫添加到其中
-  const container = new PIXI.Container();
-  container.scale.set(0.5);
-  container.addChild(dragon);
+    // 為脊椎動畫創建一個容器並將動畫添加到其中
+    const container = new PIXI.Container();
+    container.scale.set(0.5);
+    container.addChild(dragon);
 
-  app.stage.addChild(container);
+    app.stage.addChild(container);
 }
