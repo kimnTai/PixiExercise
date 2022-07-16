505bdb58734bca3969e62d3d853034a0879b8678
kimn <a9523023650@gmail.com>
2022-07-16 17:05:36 +0800

    更改 spine 測試

--- week_6_GreenSock/1213/src/app.ts
+++ week_6_GreenSock/1213/src/app.ts
@@ -13,17 +13,29 @@ document.querySelector("#app").appendChild(app.view);
 app.loader
     .add("boy", "../demo/spineboy-pro.skel")
     .add("more", "../more/moreSpin.json")
-    .add("btn", "../img/btn_active.png")
+    .add("bunny", "../img/bunny.png")
+    .add("flowerTop", "../img//flowerTop.png")
     .load((loader: PIXI.Loader, res: any) => {
         const more = new Spine(res.more.spineData);
-        more.position.set(700, 500);
-        const btn = PIXI.Sprite.from("btn");
-        btn.anchor.set(0.5);
-        const ta = (more.children[9] as PIXI.Container).children[0] as PIXI.Sprite;
-        const t = (more.children[10] as PIXI.Container).children[0] as PIXI.Sprite;
-        ta.texture = null;
-        t.texture = null;
-        t.addChild(btn);
+        more.position.set(500, 500);
+        const flowerTop = PIXI.Sprite.from("flowerTop");
+        const bunny = PIXI.Sprite.from("bunny");
+        bunny.scale.set(3);
+        flowerTop.anchor.set(0.5);
+
+        const _text = (more.children[7] as PIXI.Container).children[0] as PIXI.Sprite;
+        const add = (more.children[8] as PIXI.Container).children[0] as PIXI.Sprite;
+        const one = (more.children[9] as PIXI.Container).children[0] as PIXI.Sprite;
+        const zero = (more.children[10] as PIXI.Container).children[0] as PIXI.Sprite;
+        _text.texture = null;
+        add.texture = null;
+        one.texture = null;
+        zero.texture = null;
+        zero.addChild(flowerTop);
+        add.addChild(bunny);
+        const text = new PIXI.Text("FREE SPINS 測試", { fill: "0xffffff", fontSize: "75px" });
+        text.x = -300;
+        _text.addChild(text);
 
         more.state.setAnimation(0, "animation", true);
         app.stage.addChild(more);
