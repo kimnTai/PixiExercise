9047c15e65646e638036cb77eb09f0ca814c0ff9
kimn <a9523023650@gmail.com>
2022-07-31 23:20:43 +0800

    dev

--- week_6_GreenSock/1213/src/app.ts
+++ week_6_GreenSock/1213/src/app.ts
@@ -16,9 +16,23 @@ app.loader
     .add("bunny", "../img/bunny.png")
     .add("flowerTop", "../img//flowerTop.png")
     .load((loader: PIXI.Loader, res: any) => {
-        const more = new Spine(res.more.spineData);
+        const _more = new Spine(res.more.spineData);
+        const more = new Proxy(_more, {
+            get(target: any, p: string | symbol) {
+                //console.log(p);
+                return target[p];
+            },
+            set(target: any, p: string, value: any) {
+                if (!["worldAlpha", "_boundsID", "_textureID", "_transformID", "uvs", "lastTime"].includes(p)) {
+                    console.log(p, value);
+                }
+                target[p] = value;
+                return true;
+            },
+        });
         more.position.set(500, 500);
         const flowerTop = PIXI.Sprite.from("flowerTop");
+
         const bunny = PIXI.Sprite.from("bunny");
         bunny.scale.set(3);
         flowerTop.anchor.set(0.5);
@@ -28,11 +42,11 @@ app.loader
         const one = (more.children[9] as PIXI.Container).children[0] as PIXI.Sprite;
         const zero = (more.children[10] as PIXI.Container).children[0] as PIXI.Sprite;
         _text.texture = null;
-        add.texture = null;
+        add.texture = PIXI.Texture.from("bunny");
         one.texture = null;
         zero.texture = null;
         zero.addChild(flowerTop);
-        add.addChild(bunny);
+        //add.addChild(bunny);
         const text = new PIXI.Text("FREE SPINS 測試", { fill: "0xffffff", fontSize: "75px" });
         text.x = -300;
         _text.addChild(text);
@@ -40,3 +54,7 @@ app.loader
         more.state.setAnimation(0, "animation", true);
         app.stage.addChild(more);
     });
+
+let isSpace = false;
+window.addEventListener("keypress", (e) => (isSpace = e.code === "Space"));
+window.addEventListener("keyup", () => (isSpace = false));
--- work/vite-project/src/main2.ts
+++ work/vite-project/src/main2.ts
@@ -1,13 +1,22 @@
 import * as PIXI from "pixi.js";
-import { Power0, TweenMax } from "gsap";
 
 const app = new PIXI.Application({ width: 1200, height: 600, backgroundColor: 0x1099bb });
 document.querySelector("#app")?.appendChild(app.view);
 
-app.loader
-    .add("line", "../img/FX_Line-2.png")
-    .add("line1", "../img/pic_main_line_11.png")
-    .add("rope2", "../img/rope2.png")
-    .load(() => {});
+app.loader.load(() => {
+    // 畫圓
+    let i = -Math.PI / 2;
+    const graphics = new PIXI.Graphics();
+    const fn = () => {
+        i += (2 * Math.PI) / 300;
+        graphics.clear();
+        graphics.lineStyle(5, 0xaa00bb, 1);
+        graphics.arc(600, 100, 50, -Math.PI / 2, i);
+        graphics.endFill();
+    };
+    app.ticker.add(fn);
+    setTimeout(() => app.ticker.remove(fn), 5050);
+    app.stage.addChild(graphics);
+});
 
 export { app };
