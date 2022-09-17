7dcd050e74de5714898be73811a549b159cd408a
kimn <a9523023650@gmail.com>
2022-09-17 20:58:22 +0800

    dev

--- week_1_pixi/pixi-vite3/src/app.ts
+++ week_1_pixi/pixi-vite3/src/app.ts
@@ -1,18 +1,25 @@
 import * as PIXI from "pixi.js";
+import * as pixi_spine from "pixi-spine";
 
 export default class App extends PIXI.Application {
     constructor() {
-        super({
-            width: 1200,
-            height: 750,
-            antialias: true,
-            resolution: 1,
+        super({ width: 1200, height: 800 });
+        this._init().then(() => {
+            const spine = new pixi_spine.Spine(<any>this.loader.resources["effect"].spineData);
+            const [_in, out, score] = spine.spineData.animations.map(({ name }) => name);
+            spine.hackTextureBySlotName("01_pic/coin3", PIXI.Texture.from("blue"));
+            spine.scale.set(0.5);
+            spine.position.set(600, 400);
+            this.stage.addChild(spine);
+            spine.update(0);
+
+            // spine.state.addAnimation(0, _in, false, 0);
+            // spine.state.addAnimation(0, score, true, 0);
         });
-        this._init().then(() => {});
     }
 
-    async _init(): Promise<unknown> {
-        return new Promise((resolve) => {
+    async _init() {
+        await new Promise((resolve) => {
             document.querySelector("#app")?.appendChild(this.view);
             this.loader
                 .add("red", "../img/pic_main_line_10.png")
@@ -20,6 +27,8 @@ export default class App extends PIXI.Application {
                 .add("blue", "../img/pic_main_line_5.png")
                 .add("mask", "../img/pic_mg_reelMaskSmall.png")
                 .add("line", "../img/FX_Line-2.png")
+                .add("boy", "../demo/spineboy-pro.skel")
+                .add("effect", "../demo/01_json/effect_style136.json")
                 .load(resolve);
         });
     }
